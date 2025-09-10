492a2d6b7cb71686822b03ff867e1f76dcd07886
Matheus Santana <100173811+matssgit@users.noreply.github.com>
2025-09-10 15:24:46 -0300

    novo-curso-de-react

---
+++ primeira-pagina/index.jsx
@@ -0,0 +1,15 @@
+const element = <h1>Olá, mundo!</h1>
+
+const anotherElement = (
+    <div>
+        <h1>Texto</h1>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+        <h2></h2>
+    </div>
+)
