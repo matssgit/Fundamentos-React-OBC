9de450290b76b62b52ef114fb44414f10dad4537
Matheus Santana <100173811+matssgit@users.noreply.github.com>
2025-09-10 21:22:58 -0300

    updates react

---
+++ exercicio-1/src/App.jsx
@@ -0,0 +1,41 @@
+export default function App() {
+  return (
+    <div className="app">
+      <>
+        <img src="/react.jpg" alt="React Logo" />
+
+        <h1>React</h1>
+        <p>A biblioteca para interfaces de usuário web e nativas.</p>
+        <button>Aprenda React</button>
+        <button>Referência da API</button>
+        <hr/>
+      </>
+
+      <>
+        <h2>Crie interfaces de usuário de componentes</h2>
+        <p>React permite que você construa interfaces de usuário a partir de pedaços individuais chamados componentes</p>
+        <hr/>
+      </>
+
+      <>
+        <h2>Escreva componentes com código e marcação</h2>
+        <p>Componentes React são funções JavaScript. A sintaxe de marcação é chamada de JSX. É uma extensão da sintaxe JavaScript popularizada pelo React.</p>
+        <hr/>
+      </>
+      
+      <>
+        <h2>Próximos passos</h2>
+        <ul>
+          <li>Uso de dados dinâmicos do JSX</li>
+          <li>Criação de novos componentes</li>
+          <li>Estilização de componentes</li>
+          <li>Reutilização de componentes</li>
+          <li>Uso de props e children</li>
+          <li>Uso de eventos do JavaScript</li>
+        </ul>
+      </>
+      
+    </div>
+    
+  )
+}
--- primeira-pagina/index.jsx
+++ primeira-pagina/index.jsx
@@ -1,15 +1,13 @@
-const element = <h1>Olá, mundo!</h1>
 
-const anotherElement = (
-    <div>
-        <h1>Texto</h1>
-        <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-        </ul>
-        <h2></h2>
-    </div>
-)
+function PrimaryButton() {
+    return <button>Meu botão</button>
+} 
+
+function Container() {
+    return (
+        <div className="">
+            <h1></h1>
+            <p></p>
+        </div>
+    )
+}
