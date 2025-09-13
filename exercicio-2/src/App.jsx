fced825dd127f5ebfce7412810a963bea4ab1936
Matheus Santana <100173811+matssgit@users.noreply.github.com>
2025-09-12 22:37:59 -0300

    usando-componentes

--- exercicio-2/src/App.jsx
+++ exercicio-2/src/App.jsx
@@ -1,9 +1,15 @@
 import Card from "./components/Card";
+import swPosterImg from "../public/sw-poster.jpg";
+import esbPosterImg from "../public/esb-poster.jpg";
+import rotjPosterImg from "../public/rotj-poster.jpg";
 
 export default function App() {
 	return (
-		<div>
-			<Card />
-		</div>
+		<>
+			<h1>Exercício 2</h1>
+			<Card title="Pôster: Star Wars (1977)" poster={swPosterImg} />
+			<Card title="Pôster: Empire Strikes Back (1980)" poster={esbPosterImg} />
+			<Card title="Pôster: Return of the Jedi (1983)" poster={rotjPosterImg} />
+		</>
 	);
 }
--- exercicio-2/src/components/Card/index.jsx
+++ exercicio-2/src/components/Card/index.jsx
@@ -1,12 +1,12 @@
-import posterImg from "../../../public/star-wars-poster.jpg";
+import Button from "../Button";
 import styles from "./styles.module.css";
 
-export default function Card() {
+export default function Card({ title, poster }) {
 	return (
 		<div className={styles.container}>
-			<img className={styles.poster} src={posterImg} alt="Star Wars poster" />
+			<img className={styles.poster} src={poster} alt={title} />
 			<div>
-				<h2 className={styles.title}>Pôster do filme Star Wars</h2>
+				<h2 className={styles.title}>{title}</h2>
 				<p className={styles.description}>
 					Um pôster decorativo épico do filme Star Wars, com moldura de MDF e
 					tamanho A3. Uma ótima recordação de um dos mais icônicos filmes de
@@ -14,7 +14,7 @@ export default function Card() {
 					magia de Star Wars para qualquer lugar que você decidir pendurar. Não
 					perca a chance de adicionar essa linda memória ao seu acervo!
 				</p>
-				<button className={styles.button}>Comprar agora</button>
+				<Button text="Comprar agora" />
 			</div>
 		</div>
 	);
