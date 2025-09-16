29060e3b08b64b0dddef27011b16bdc3f798dc2a
Matheus Santana <100173811+matssgit@users.noreply.github.com>
2025-09-16 18:05:33 -0300

    conhecendo usestates

--- exercicio-3/src/components/Profile/index.jsx
+++ exercicio-3/src/components/Profile/index.jsx
@@ -2,21 +2,37 @@ import Title from "../Title";
 import ProfileSection from "./ProfileSection";
 import styles from "./styles.module.css";
 import LinkButton from "../LinkButton";
+import { useState } from "react";
 
 export default function Profile(props) {
+	// [valor, funcaoModificadora]	= useState(valorInicial)
+	const [followText, setFollowText] = useState("Follow");
+
+	function handleClick(ev) {
+		alert("Você agora está seguindo!");
+		setFollowText("Following");
+	}
+
 	return (
 		<div className={styles.container}>
 			<img className={styles.avatar} src={props.avatar} alt={props.name} />
 			<Title>
 				<span>Name: </span>
 				{props.name}
-				<button>Follow</button>
+				<button onClick={handleClick} className={styles.followButton}>
+					{followText}
+				</button>
 			</Title>
 			<ProfileSection>{props.bio}</ProfileSection>
 			<ProfileSection>{props.phone}</ProfileSection>
 			<ProfileSection>{props.email}</ProfileSection>
-			<ProfileSection>
-				<div className={styles.links}>
+			<ProfileSection
+				className={styles.links}
+				id="links-section"
+				data-test="some value"
+				arial-label="social links"
+			>
+				<div>
 					<LinkButton href={props.githubUrl} target="_blank">
 						GitHub
 					</LinkButton>
