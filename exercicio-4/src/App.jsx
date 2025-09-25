99fee2396f7dfbc992c33da5c74842e7487bdfb3
Matheus Santana <100173811+matssgit@users.noreply.github.com>
2025-09-25 12:31:03 -0300

    exercicio 4 e controlando inputs, compartilhando estado entre componentes

--- exercicio-4/src/App.jsx
+++ exercicio-4/src/App.jsx
@@ -1,16 +1,17 @@
 import { useState } from "react";
 import "./App.css";
+import Input from "./components/Input";
 
 function App() {
 	const [password, setPassword] = useState("");
 	const [copyText, setCopyText] = useState("Copiar");
+	const [passwordSize, setPasswordSize] = useState(12);
 
 	function generate() {
 		const characters =
 			"'1234567890-=!@#$%¨&*()_+qwertyuiop[asdfghjklç~]zxcvbnm,.;/QWERTYUIOP{ASDFGHJKLÇ^}ZXCVBNM<>:?";
-		const length = 12;
 		let newPassword = "";
-		for (let i = 0; i < length; i++) {
+		for (let i = 0; i < passwordSize; i++) {
 			const position = Math.floor(Math.random() * characters.length);
 			newPassword += characters[position];
 		}
@@ -26,7 +27,13 @@ function App() {
 	return (
 		<>
 			<h1>Gerador de Senhas</h1>
-			<button onClick={generate}>Gerar!</button>
+			<div>
+				<label htmlFor="passwordSize">Tamanho: </label>
+				<Input passwordSize={passwordSize} setPasswordSize={setPasswordSize} />
+			</div>
+			<button onClick={generate}>
+				Gerar senha de {passwordSize} caracteres!
+			</button>
 			<button onClick={copyToClipboard}>{copyText}</button>
 			<div>{password}</div>
 		</>
---
+++ exercicio-4/src/components/Input.jsx
@@ -0,0 +1,18 @@
+import propTypes from "prop-types";
+
+Input.propTypes = {
+	passwordSize: propTypes.number.isRequired,
+	setPasswordSize: propTypes.func.isRequired,
+};
+
+export default function Input(props) {
+	return (
+		<input
+			type="number"
+			id="passwordSize"
+			min={1}
+			value={props.passwordSize}
+			onChange={(ev) => props.setPasswordSize(ev.target.value)}
+		/>
+	);
+}
